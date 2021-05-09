a882956e4baf83dc5982ffaa2a5ba8da3da8c279
Hugo <noppanat.num@dome.tu.ac.th>
2021-05-10 00:12:30 +0700

    Update Home.js

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,17 +1,34 @@
 
-import React from 'react';
-import Carousel from 'nuka-carousel';
-
+import React, {Component} from 'react';
+import PostsData from '../Data/PostsData'
 import Card from './Card';
 
-function Home() {
+class Home extends Component {
+constructor() {
+  super();
+    
+  this.state = {
+    posts: {}
+  }
+}
+componentWillMount() {
+  this.setState({
+    posts: PostsData
+    
+  });
+}
+render(){
+  console.log(this.componentWillMount);
+  console.log(this.state.posts);
   return (
     <>
       <div className = 'container'>
-        <Card></Card>
-        <h1>bosss</h1>
+      {Object
+          .keys(this.state.posts)
+          .map(key => <Card key={key} index={key} details={this.state.posts[key]}/>)}
       </div>
     </>
   );
+}
 }
 export default Home;
