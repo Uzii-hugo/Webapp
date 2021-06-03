f328be61d8d85cbebc9abbec2ce56208ac1cb741
teyspeedSCI <bossbaby_2005@hotmail.com>
2021-06-03 23:48:23 +0700

    ed

--- Client/src/home/NavOut.js
+++ Client/src/home/NavOut.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+// import React from 'react';
+// import {NavLink} from 'react-router-dom';
 
-const NavOut = () =>{
+// const NavOut = () =>{
 
-    return (
-    <header className="homehead">
-        <nav className="header">
-            <NavLink exact activeClassName="active" to="/">หน้าหลัก</NavLink>
-            <NavLink activeClassName="active" to="/dashboard">ดูดวง</NavLink>
-            <NavLink activeClassName="active" to="/SignIn" className= "rightbar">LogIn</NavLink>
-        </nav>
-    </header>)
+//     return (
+//     <header className="homehead">
+//         <nav className="header">
+//             <NavLink exact activeClassName="active" to="/">หน้าหลัก</NavLink>
+//             <NavLink activeClassName="active" to="/dashboard">ดูดวง</NavLink>
+//             <NavLink activeClassName="active" to="/SignIn" className= "rightbar">LogIn</NavLink>
+//         </nav>
+//     </header>)
     
-}
+// }
 
-export default NavOut
+// export default NavOut
